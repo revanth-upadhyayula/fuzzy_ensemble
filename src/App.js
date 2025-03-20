b069c651f23178c4050b4192fe9c2a3a1ddd5ea6
Revanth Upadhyayula <169019945+revanth-upadhyayula@users.noreply.github.com>
2025-03-20 23:35:35 +0530

    basic web page done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
+import TechnicalProcess from './components/TechnicalProcess';
+import Footer from './components/Footer';
+import Research from './components/Research';
+import Team from './components/Team';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <HowItWorks />
+                <TechnicalProcess />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/research" element={<Research />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
